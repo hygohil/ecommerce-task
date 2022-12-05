365dc1b4e157d1a2c9d3ed51881923d1bc4f45fb
Harpalsinh Gohil <harpalsinh.gohil4578@gmail.com>
2022-12-05 14:33:56 +0530

    add get user controller

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,11 +1,24 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Controller, Get, Response } from '@nestjs/common';
 import { User } from '@prisma/client';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+
+import { AuthUser } from '../auth/auth.user.decorator';
 
 import { UserService } from './user.service';
+import { UserResponseDTO } from './user.dto';
 
 @ApiTags('users')
 @Controller('/users')
 export class UserController {
   constructor(private userService: UserService) {}
+
+  @Get('/')
+  @ApiOperation({ description: 'Get user data' })
+  @ApiResponse({ type: UserResponseDTO })
+  async getUser(
+    @AuthUser() user: User,
+    @Response() res,
+  ): Promise<UserResponseDTO> {
+    return res.status(200).send({ user });
+  }
 }
